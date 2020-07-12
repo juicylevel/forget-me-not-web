c875f4106aede484f9f29accc709b8dd901afd45
Yuri Rykov <rykov87yuri@gmail.com>
2020-07-12 13:31:28 +0300

    app layout

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Layout, Space } from 'antd';
+import logoSvg from 'images/forget-me-not.svg';
+import 'antd/dist/antd.css';
+
+const { Footer, Sider, Content } = Layout;
+
+const HEADER_HEIGHT = '64px';
+
+const FullHeightLayout = styled(Layout)`
+    & {
+        height: 100%;
+    }
+`;
+
+const LogoWrapper = styled(Link)`
+    display: block;
+`;
+
+const Logo = styled.div`
+    width: ${HEADER_HEIGHT};
+    height: ${HEADER_HEIGHT};
+    background-image: url(${logoSvg});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 42px 42px;
+`;
+
+const LogoLabel = styled.span`
+    font-size: 1.6rem;
+`;
+
+const Header = styled(Layout.Header)`
+    height: ${HEADER_HEIGHT};
+    background-color: white;
+`;
 
 export default () => (
-    <h3>forget me not</h3>
+    <Router>
+        <FullHeightLayout>
+            <Sider width="320">
+                <LogoWrapper to="/">
+                    <Space>
+                        <Logo />
+                        <LogoLabel>
+                            Forget-me-not
+                        </LogoLabel>
+                    </Space>
+                </LogoWrapper>
+            </Sider>
+            <Layout>
+                <Header>Header</Header>
+                <Content>
+                    
+                </Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </FullHeightLayout>
+    </Router>
 );
