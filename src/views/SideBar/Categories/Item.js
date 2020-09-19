ff57e8c6b85ade25069f0422ed9874be5099dbc7
Yuri Rykov <rykov87yuri@gmail.com>
2020-09-19 11:36:39 +0300

    added category menu sandwich

--- src/views/SideBar/Categories/Item.js
+++ src/views/SideBar/Categories/Item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Box from '@material-ui/core/Box';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Icon from './Icon';
 
 const IconShape = styled(Avatar)`
@@ -13,6 +16,18 @@ const IconShape = styled(Avatar)`
         background-color: ${color};
         color: white;
     `}
+`;
+
+const ListItemContainer = styled.li`
+    .edit-button {
+        opacity: 0;
+        transition: opacity 200ms;
+    }
+    &:hover {
+        .edit-button {
+            opacity: 1;
+        }
+    }
 `;
 
 const Item = ({
@@ -26,6 +41,7 @@ const Item = ({
     return (
         <ListItem 
             button 
+            ContainerComponent={ListItemContainer}
             selected={selected}
             onClick={onClick}
         >
@@ -36,7 +52,17 @@ const Item = ({
             </ListItemAvatar>
             <ListItemText primary={label} />
             <ListItemSecondaryAction>
-                <ListItemText primary={count} />
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    minWidth="70px"
+                >
+                    <IconButton className="edit-button">
+                        <MoreVertIcon />
+                    </IconButton>
+                    <span>{count}</span>
+                </Box>
             </ListItemSecondaryAction>
         </ListItem>
     );
